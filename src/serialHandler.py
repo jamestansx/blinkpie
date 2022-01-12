efa31f111cdf95cf15a64e3656f45ff9a22d0c96
James Tan <jamestansx@gmail.com>
2022-01-12 13:08:08 +0800

    Implement error handling

--- src/serialHandler.py
+++ src/serialHandler.py
@@ -1,49 +1,54 @@
+import argparse
 import json
 import logging
 from socket import gethostbyname, gethostname
+import sys
 
-from serial import Serial
+import serial
+from serialtools import SerialTools
 
-try:
-    from serialtools import *
-except Exception as e:
-    print(e)
-    input("")
-
-logger.level = logging.DEBUG
 appname = "blinkpie"
 authorname = "jamestansx"
 iplink = gethostbyname(gethostname())
 server = "https://" + iplink + ":443"
-port = Serial(port="COM2", baudrate=9600, timeout=0.1, write_timeout=0.1)
 SUCCESS_CODE = "200"
 
+logging.basicConfig(
+    format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
+    handlers=[logging.StreamHandler()],
+)
+logger = logging.getLogger()
+logger.level = logging.DEBUG
 
-def init():
+
+
+
+
+def init(st):
     print(f"IP Address: {server}")
     print("-------------------------------")
     res = input("Generate new Arduino UUID?[y/n] ")
     while res not in {"y", "n"}:
         res = input("Generate new Arduino UUID?[y/n] ")
     if res == "y":
-        uuid = genUUID()
+        uuid = st.genUUID()
         print(f"Arduino new UUID: {uuid}")
         setProfile(uuid)
     print("-------------------------------")
 
 
-def setProfile(uuid):
+def setProfile(st,uuid):
     name = input("Arduino name? ")
     description = input("Description[optional]: ")
     dicts = {
         "profile": {"UUID": uuid, "name": name, "description": description},
     }
-    return do_post(server, dicts)
+    return st.do_post(server, dicts)
 
 
-def connect(get_data: str):
+def connect(st,get_data: str):
     logger.debug(f"Connecting to Arduino: {get_data}")
-    data = do_get(server, json.loads(get_data))
+    data = st.do_get(server, json.loads(get_data))
     logger.debug(f"data {data}")
     content = json.loads(data.text)
     status = data.status_code
@@ -51,53 +56,75 @@ def connect(get_data: str):
         logger.info("Connected to device:")
         logger.info(f"Name: {content['name']}")
         logger.info(f"Description: {content['description']}")
-        do_write(port, "200")
+        st.do_write(port, "200")
         return True
     elif status == 404:
         logger.warning("Arduino is not registered.. Please setup again")
         return False
 
 
-def is_available():
+def is_available(st):
     status = ""
     while status != SUCCESS_CODE:
         logger.debug("checking handshake")
-        status = do_read(port)
+        status = st.do_read(port)
         if "profile" in status:
             if not connect(status):
                 setProfile(genUUID())
             break
 
 
-def main():
-    try:
-        while True:
-            readData = do_read(port)
+def main(st):
+    while True:
+        try:
+            readData = st.do_read(port)
             if "profile" in readData:
-                if not connect(readData):
-                    setProfile(genUUID())
+                if not connect(st,readData):
+                    setProfile(st,genUUID())
                 continue
-            is_GET = parse_data(readData)
+            is_GET = parse_data(st,readData)
             if is_GET:
                 logger.info("GET <--- SERVER")
-                getData = do_get(server, json.loads(readData))
+                getData = st.do_get(server, json.loads(readData))
                 if getData.status_code == 404:
-                    do_write(port, "404")
+                    st.do_write(port, "404")
                 else:
-                    do_write(port, getData.text)
+                    st.do_write(port, getData.text)
                 is_available()
             else:
                 logger.info("POST ---> SERVER")
-                _ = do_post(server, json.loads(readData))
-                do_write(port, SUCCESS_CODE)
-    except KeyboardInterrupt:
-        inputs = input("Exit?[y|n(restart)] ")
-        if inputs == "n":
-            main()
-        logger.info("exiting virtual ports handler...")
-        exit(0)
+                _ = st.do_post(server, json.loads(readData))
+                st.do_write(port, SUCCESS_CODE)
+        except KeyboardInterrupt:
+            inputs = input("Exit?[y|n(restart)] ")
+            if inputs == "n":
+                logger.info("restarting virtual ports handler")
+                continue
+            else:
+                logger.info("exiting virtual ports handler...")
+                exit(0)
+        except Exception as e:
+            logger.exception("Oh no! Something's wrong!")
+            continue
 
 
 if __name__ == "__main__":
-    init()
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port",nargs='?', default="COM1", help="specify port number to use (COM1)", type=str)
+    parser.add_argument("--baudrate", nargs='?', default="9600", help="specify baudrate (9600)", type=str)
+    parser.add_argument("--timeout", nargs='?', default=.1, help="specify port number to use (COMX)", type=float)
+    parser.add_argument("--write_timeout", nargs='?', default=.1, help="specify port number to use (COMX)", type=float)
+    args = parser.parse_args()
+
+    try: 
+        port = serial.Serial(
+                port=args.port, baudrate=args.baudrate, timeout=args.timeout, write_timeout=args.write_timeout
+        )
+    except Exception as e:
+        logger.exception("Port not found!")
+        input("Press Enter to continue...")
+        sys.exit(1)
+
+    tools = SerialTools()
+    init(tools)
+    main(tools)
--- src/serialtools.py
+++ src/serialtools.py
@@ -3,52 +3,52 @@ from typing import Any
 import json
 
 from requests import get, post
+from uuid import uuid4
+
 from requests.packages import urllib3
 from serial import Serial
 from urllib3.exceptions import InsecureRequestWarning
 
 urllib3.disable_warnings(InsecureRequestWarning)
 
-logging.basicConfig(
-    format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
-    handlers=[logging.StreamHandler()],
-)
-logger = logging.getLogger()
+
+class SerialTools():
+    def __init__(self):
+        logging.basicConfig(
+            format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
+            handlers=[logging.StreamHandler()],
+        )
+        self.logger = logging.getLogger()
+
+    def genUUID(self):
+        return uuid4().hex
 
 
-def genUUID():
-    from uuid import uuid4
-
-    return uuid4().hex
-
-
-def do_read(port: Serial) -> str:
-    while True:
-        read_data = port.readline().decode().strip(" \r\n\b")
-        if read_data == "":
-            continue
-        logger.debug("Read: %s", read_data)
-        return read_data
+    def do_read(self,port: Serial) -> str:
+        while True:
+            read_data = port.readline().decode().strip(" \r\n\b")
+            if not read_data:
+                continue
+            self.logger.debug("Read: %s", read_data)
+            return read_data
 
 
-def do_write(port: Serial, write_data: str):
-    port.write(f"{write_data}\r\n".encode())
-    logger.debug("Write: %s", write_data)
+    def do_write(self,port: Serial, write_data: str):
+        port.write(f"{write_data}\r\n".encode())
+        self.logger.debug("Write: %s", write_data)
 
 
-def parse_data(string) -> bool:
-    string = json.loads(string.strip(" \r\t\n$0"))
-    if isinstance(string[next(iter(string))], dict):
-        return False  # GET
-    return True  # POST
+    def parse_data(self,string) -> bool:
+        string = json.loads(string.strip(" \r\t\n$0"))
+        return False if isinstance(string[next(iter(string))], dict) else True
 
 
-def do_get(server: str, params: dict = None):
-    get_data = get(server, verify=False, params=params)
-    logger.debug("GET data: %s", get_data.text)
-    return get_data
+    def do_get(self,server: str, params: dict = None):
+        get_data = get(server, verify=False, params=params)
+        self.logger.debug("GET data: %s", get_data.text)
+        return get_data
 
 
-def do_post(server: str, post_data: dict[str, Any]):
-    logger.debug("POST: %s", post_data)
-    return post(server, verify=False, json=post_data)
+    def do_post(self,server: str, post_data: dict[str, Any]):
+        self.logger.debug("POST: %s", post_data)
+        return post(server, verify=False, json=post_data)
