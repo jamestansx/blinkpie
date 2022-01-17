2f9fb5eb827b6eb9dc36147c97e2e2ab36661334
James Tan <jamestansx@gmail.com>
2022-01-17 11:57:39 +0800

    Refactor code

--- src/serialtools.py
+++ src/serialtools.py
@@ -1,10 +1,9 @@
+import json
 import logging
 from typing import Any
-import json
-
-from requests import get, post
 from uuid import uuid4
 
+import requests
 from requests.packages import urllib3
 from serial import Serial
 from urllib3.exceptions import InsecureRequestWarning
@@ -12,19 +11,18 @@ from urllib3.exceptions import InsecureRequestWarning
 urllib3.disable_warnings(InsecureRequestWarning)
 
 
-class SerialTools():
+class SerialTools:
     def __init__(self):
         logging.basicConfig(
             format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
             handlers=[logging.StreamHandler()],
         )
-        self.logger = logging.getLogger()
+        self.logger = logging.requests.getLogger()
 
     def genUUID(self):
         return uuid4().hex
 
-
-    def do_read(self,port: Serial) -> str:
+    def do_read(self, port: Serial) -> str:
         while True:
             read_data = port.readline().decode().strip(" \r\n\b")
             if not read_data:
@@ -32,23 +30,19 @@ class SerialTools():
             self.logger.debug("Read: %s", read_data)
             return read_data
 
-
-    def do_write(self,port: Serial, write_data: str):
+    def do_write(self, port: Serial, write_data: str):
         port.write(f"{write_data}\r\n".encode())
         self.logger.debug("Write: %s", write_data)
 
-
-    def parse_data(self,string) -> bool:
+    def parse_data(self, string) -> bool:
         string = json.loads(string.strip(" \r\t\n$0"))
         return False if isinstance(string[next(iter(string))], dict) else True
 
-
-    def do_get(self,server: str, params: dict = None):
-        get_data = get(server, verify=False, params=params)
+    def do_get(self, server: str, params: dict = None):
+        get_data = requests.get(server, verify=False, params=params)
         self.logger.debug("GET data: %s", get_data.text)
         return get_data
 
-
-    def do_post(self,server: str, post_data: dict[str, Any]):
+    def do_post(self, server: str, post_data: dict[str, Any]):
         self.logger.debug("POST: %s", post_data)
-        return post(server, verify=False, json=post_data)
+        return requests.post(server, verify=False, json=post_data)
